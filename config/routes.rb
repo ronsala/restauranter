8cabdb4cb698b0aa24bd4b22fc0136b17ef605a6
RonSala <ron.sala@gmail.com>
2021-05-23 18:05:27 -0400

    add top-level menus route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :menus
+
       resources :restaurants do
         resources :menus do
           resources :sections do

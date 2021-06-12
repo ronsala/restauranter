081d5c3960f6a756388a80a3978a2e3b7172dc4f
RonSala <ron.sala@gmail.com>
2021-06-12 15:11:53 -0400

    update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, :city, :state, presence: true
+  validates :email, uniqueness: true
+  has_secure_password
   has_many :restaurants
 end

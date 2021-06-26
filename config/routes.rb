f4ea19dd0091f7d101bc2a35c88243c36fd14dc4
RonSala <ron.sala@gmail.com>
2021-06-26 17:36:30 -0400

    fix typos in routes

--- config/routes.rb
+++ config/routes.rb
@@ -44,10 +44,10 @@ Rails.application.routes.draw do
       end
       
       resources :orders do
-        resources :order_items, only %i[ index show ]
+        resources :order_items, only: %i[ index show ]
       end
 
-      resources :order_items, only %i[ create ]
+      resources :order_items, only: %i[ create ]
 
       post 'login', to: 'users#login'
     end
